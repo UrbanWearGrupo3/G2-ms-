41deb57b07e06fe4c3bc7d6973ca1e223ed133cb
Jaqueline Atienza <jaquibatienza@gmail.com>
2026-06-17 16:56:25 -0300

    se añadio la depencia lombok ademas de la creacion de la creacion de las entity

--- src/main/java/com/grupo3/tienda_ropa/producto/entity/ProductEntity.java
+++ src/main/java/com/grupo3/tienda_ropa/producto/entity/ProductEntity.java
@@ -1,5 +1,27 @@
 package com.grupo3.tienda_ropa.producto.entity;
 
-public class ProductEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="DetallesProductos")
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Number precio;
+    private String talle;
+    private String color;
+    private String imagen;
+    private Number stock;         
 }
