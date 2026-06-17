d66289c0e96ba5b71f84eea13d0c6494c46f261c
Jaqueline Atienza <jaquibatienza@gmail.com>
2026-06-17 17:05:12 -0300

    los controladores estan correctamente configurados y funcionando

--- src/main/java/com/grupo3/tienda_ropa/producto/controllers/ProductoController.java
+++ src/main/java/com/grupo3/tienda_ropa/producto/controllers/ProductoController.java
@@ -1,10 +1,31 @@
 package com.grupo3.tienda_ropa.producto.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.grupo3.tienda_ropa.producto.entity.ProductEntity;
+import com.grupo3.tienda_ropa.producto.service.ProductoService;
 
 @RestController
-@RequestMapping("/producto")
+@RequestMapping("/productos")
 public class ProductoController {
 
+    private final ProductoService service;
+
+    public ProductoController(ProductoService service) {
+        this.service = service;
+    }
+
+    //subir productos
+    @PostMapping
+    public ProductEntity guardar(@RequestBody ProductEntity producto) {
+        return service.guardar(producto);
+    }
+
+    //mostrar todos los productos
+    @GetMapping
+    public List<ProductEntity> listar() {
+        return service.listar();
+    }
 }
